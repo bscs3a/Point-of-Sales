c4ed4a089c2b082e277a9d333caf3929d5763a7d
Isarakyun <119465506+Isarakyun@users.noreply.github.com>
2024-03-24 23:07:34 +0800

    Update hr.employees.update.php

--- public/humanResources/views/hr.employees.update.php
+++ public/humanResources/views/hr.employees.update.php
@@ -496,7 +496,7 @@
                       <div>
                       </div>
                       <div class="flex flex-row mt-8 justify-center">
-                        <button type="submit" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">Save</button>
+                        <button type="submit" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">Update</button>
                         <button route="/hr/employees" type="button" class="focus:outline-none text-gray-700 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">Cancel</button>
                       </div>
                       </form>
