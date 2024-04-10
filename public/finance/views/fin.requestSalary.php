1f7d4138e311092d93b378f2e66f33cec0e12312
Drex <94889971+bleckmembe@users.noreply.github.com>
2024-04-11 00:37:41 +0800

    Tab Titles for Requests

--- public/finance/views/fin.requestSalary.php
+++ public/finance/views/fin.requestSalary.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ledger</title>
+    <title>Salary Requests</title>
     <link href="./../src/tailwind.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon/fonts/remixicon.css">
 </head>
