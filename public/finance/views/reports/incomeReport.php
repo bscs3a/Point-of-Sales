1c69db32f31166c8fd9d231f6eced9f5afc51f09
bakasibruceto <abla09027@gmail.com>
2024-04-11 01:37:05 +0800

    styled incom report

--- public/finance/views/reports/incomeReport.php
+++ public/finance/views/reports/incomeReport.php
@@ -1,52 +1,87 @@
-<?php 
-    // require_once '../../functions/reportGeneration/IncomeReport.php'; -- might change into ajax
-    $today = new DateTime();
-    $lastDayOfMonth = new DateTime($today->format('Y-m-t'));
+<?php
+// require_once '../../functions/reportGeneration/IncomeReport.php'; -- might change into ajax
+$today = new DateTime();
+$lastDayOfMonth = new DateTime($today->format('Y-m-t'));
 
-    if ($today < $lastDayOfMonth) {
-        $today->modify('-1 month');
-    }
+if ($today < $lastDayOfMonth) {
+    $today->modify('-1 month');
+}
 
-    $year = $today->format('Y');
-    $month = $today->format('n');
-    $monthName = $today->format('F');
-    if (isset($_SESSION['postdata']['year']) && isset($_SESSION['postdata']['month'])){
-        $year = $_SESSION['postdata']['year'];
-        $month =$_SESSION['postdata']['month'];
-    }
+$year = $today->format('Y');
+$month = $today->format('n');
+$monthName = $today->format('F');
+if (isset($_SESSION['postdata']['year']) && isset($_SESSION['postdata']['month'])) {
+    $year = $_SESSION['postdata']['year'];
+    $month = $_SESSION['postdata']['month'];
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Income Report</title>
+    <style>
+        ul {
+            list-style-type: none;
+        }
+
+        header {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 10px 20px;
+        }
+    </style>
 </head>
-<style>
-    ul {
-        list-style-type: none;  
-    }
-    header{
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: 10px 20px;
-    }
-</style>
+
 
 <body>
     <header>
         <div>
-            <span>Income Statement</span>
-            <span>
-                <?php echo "$monthName $year"?>    
+            <span style="font-weight: bold; font-size: 2em;">Income Statement</span>
+            <br>
+            <span style="font-weight: bold; opacity: 0.5; font-size: 1.17em;">
+                <?php echo "$monthName $year" ?>
             </span>
+            <span>
+                <table style="width: 100%; border-spacing: 0;">
+                    <tr style="background-color: #FFEEA5; ">
+                        <th>Assets</th>
+                        <th>(Dr) Amount</th>
+                    </tr>
+                    <tr>
+                        <td>Fixed Assets</td>
+                        <td style="text-align: right;">0</td>
+                    </tr>
+                    <tr>
+                        <td>Current Assets</td>
+                        <td style="text-align: right;">Dr 0</td>
+                    </tr>
+                    <tr>
+                        <td>Investments</td>
+                        <td style="text-align: right;">0</td>
+                    </tr>
+                    <tr>
+                        <td style="font-weight: bold;">Total Assets</td>
+                        <td style="text-align: right; font-weight: bold;">0</td>
+                    </tr>
+                </table>
+            </span>
+
+        </div>
+        <div>
+
         </div>
         <!-- <img src="..\..\img\logo_reports.png" alt=""> -->
     </header>
     <?php
-        // echo generateIncomeReport($year, $month);
+    // echo generateIncomeReport($year, $month);
     ?>
+
+
 </body>
+
 </html>
