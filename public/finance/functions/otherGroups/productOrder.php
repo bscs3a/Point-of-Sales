30e8b8f710cf17cae71e9c45c5540a2de9f1a09d
aries joseph tagle <ajtagle12@gmail.com>
2024-04-11 17:15:20 +0800

    tried to start something

---
+++ public/finance/functions/otherGroups/productOrder.php
@@ -0,0 +1,12 @@
+<?php
+require_once "public/finance/functions/reportGeneration/incomeReport.php";
+
+function giveCurrentPondo(){
+    const FINANCE = 0.2;
+
+
+    $base = calculateNetSalesOrLoss($year, $month);
+    
+}
+
+?>
