5562e79ba8c0de7fa9bb2615027a9868a0579d1b
aries joseph tagle <ajtagle12@gmail.com>
2024-05-14 12:41:04 +0800

    remove problem guarding login

--- index.php
+++ index.php
@@ -3,79 +3,89 @@
 // database conncetion
 require_once './src/dbconn.php';
 
+//     $base_url = '/Finance'; // Define your base URL here
+//     if (isset($_SESSION['user'])) {
+//         $role = $_SESSION['user']['role'];
+//         $current_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); // get the current path
+
+//         // Define the keywords for each role
+//         $keywords = [
+//             'Product Order' => ['/po/'],
+//             'Human Resources' => ['/hr/'],
+//             'Point of Sales' => ['/sls/'],
+//             'Inventory' => ['/inv/'],
+//             'Finance' => ['/fin/'],
+//             'Delivery' => ['/dlv/'],
+//         ];
+
+//         // Check if the role exists in the keywords array
+//         if (isset($keywords[$role])) {
+//             $allowed = false;
+//             foreach ($keywords[$role] as $keyword) {
+//                 if (strpos($current_path, $keyword) !== false) {
+//                     // The path contains one of the keywords for this role
+//                     $allowed = true;
+//                     break;
+//                 }
+//             }
+
+//             if (!$allowed) {
+//                 // The path does not contain any of the keywords for this role
+//                 // Redirect to a default page
+//                 $target_path = '';
+//                 switch ($role) {
+//                     case 'Product Order':
+//                         $target_path = "$base_url/po/dashboard";
+//                         break;
+//                     case 'Human Resources':
+//                         $target_path = "$base_url/hr/employees";
+//                         break;
+//                     case 'Point of Sales':
+//                         $target_path = "$base_url/sls/Dashboard";
+//                         break;
+//                     case 'Inventory':
+//                         $target_path = "$base_url/inv/dashboard";
+//                         break;
+//                     case 'Finance':
+//                         $target_path = "$base_url/fin/dashboard";
+//                         break;
+//                     case 'Delivery':
+//                         $target_path = "$base_url/dlv/dashboard";
+//                         break;
+//                     default:
+//                         $target_path = "$base_url/";
+//                         break;
+//                 }
+//             }
+//         } else {
+//             session_destroy();
+//             // The role does not exist in the keywords array
+//             // Redirect to a default page
+//             $target_path = "$base_url/";
+//             if ($current_path != $target_path) {
+//                 header("Location: $target_path");
+//                 exit();
+//             }
+//         }
+//     } else {
+//         // If the user is not logged in, redirect to the login page
+//         $target_path = "$base_url/";
+//         if ($current_path != $target_path) {
+//             header("Location: $target_path");
+//             exit();
+//         }
+//     }
+// }
+
+
 // router
 require_once './router.php';
 
 // routes
 require_once './web.php';
 
-//for login guarding
-if ($_SERVER['REQUEST_METHOD'] != 'POST') {
-    if (isset($_SESSION['user'])) {
-        $role = $_SESSION['user']['role'];
-        $url = $_SERVER['REQUEST_URI']; // get the current URL
 
-        // Define the keywords for each role
-        $keywords = [
-            'Product Order' => ['/po/'],
-            'Human Resources' => ['/hr/'],
-            'Point of Sales' => ['/sls/'],
-            'Inventory' => ['/inventory/'],
-            'Finance' => ['/fin/'],
-            'Delivery' => ['/delivery/'],
-        ];
 
-        // Check if the role exists in the keywords array
-        if (isset($keywords[$role])) {
-            $allowed = false;
-            foreach ($keywords[$role] as $keyword) {
-                if (strpos($url, $keyword) !== false) {
-                    // The URL contains one of the keywords for this role
-                    $allowed = true;
-                    break;
-                }
-            }
-
-            if (!$allowed) {
-                // The URL does not contain any of the keywords for this role
-                // Redirect to a default page
-                switch ($role) {
-                    case 'Product Order':
-                        header("Location: /po/dashboard");
-                        break;
-                    case 'Human Resources':
-                        header("Location: /hr/dashboard");
-                        break;
-                    case 'Point of Sales':
-                        header("Location: /sls/dashboard");
-                        break;
-                    case 'Inventory':
-                        header("Location: /inventory/dashboard");
-                        break;
-                    case 'Finance':
-                        header("Location: /fin/dashboard");
-                        break;
-                    case 'Delivery':
-                        header("Location: /delivery/dashboard");
-                        break;
-                    default:
-                        header("Location: /");
-                        break;
-                }
-                exit();
-            }
-        } else {
-            // The role does not exist in the keywords array
-            // Redirect to a default page
-            header("Location: /");
-            exit();
-        }
-    } else {
-        // If the user is not logged in, redirect to the login page
-        header("Location: /");
-        exit();
-    }
-}
 // header("Location: /Finance/");
 
 
