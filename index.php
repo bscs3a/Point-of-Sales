04a5128cca60a0abb307331c151720d5693cd562
aries joseph tagle <ajtagle12@gmail.com>
2024-05-14 18:00:03 +0800

    redirects if page not found

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
  session_start();
+
 // database conncetion
 require_once './src/dbconn.php';
 
@@ -95,8 +96,9 @@ Router::post('/login', function(){
     $stmt = $conn->prepare("SELECT * FROM account_info WHERE username = :username");
     $stmt->bindParam(':username', $username);
     $stmt->execute();
-
     $user = $stmt->fetch();
+
+    $base_url = 'Finance'; // Define your base URL here
     if ($user && password_verify($password, $user['password'])) {
         $_SESSION['user'] = array();
         // Password is correct
@@ -107,38 +109,39 @@ Router::post('/login', function(){
 
         //redirects to the right page
         if ($user['role'] == 'Product Order') {
-            header("Location: /Finance/po/dashboard");
+            header("Location: /$base_url/po/dashboard");
             exit();
         } 
         if ($user['role'] == 'Human Resources') {
-            header("Location: /Finance/hr/employees");
+            header("Location: /$base_url/hr/dashboard");
             exit();
         } 
         if ($user['role'] == 'Point of Sales') {
-            header("Location: /Finance/sls/Dashboard");
+            header("Location: /$base_url/sls/Dashboard");
             exit();
         } 
         if ($user['role'] == 'Inventory') {
-            header("Location: /Finance/inv/main");
+            header("Location: /$base_url/inv/main");
             exit();
         } 
         if ($user['role'] == 'Finance') {
-            header("Location: /Finance/fin/dashboard");
+            header("Location: /$base_url/fin/dashboard");
             exit();
         } 
         if ($user['role'] == 'Delivery') {
-            header("Location: /Finance/dlv/dashboard");
+            header("Location: /$base_url/dlv/dashboard");
             exit();
         } 
     } else {
-        header("Location: /Finance/?error=1");
+        header("Location: /$base_url/?error=1");
         exit();
     }
 });
 
 Router::post('/logout', function(){
     session_destroy();
-    header("Location: /Finance/");
+    $base_url = 'Finance'; // Define your base URL here
+    header("Location: /$base_url/");
     exit();
 });
 
