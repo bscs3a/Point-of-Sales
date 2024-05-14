f08408e3cecadaefa36e9058e4067a251a8387d8
aries joseph tagle <ajtagle12@gmail.com>
2024-05-14 13:49:35 +0800

    added login logic on web.php

--- web.php
+++ web.php
@@ -21,6 +21,29 @@ $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $basePath = '/Finance'; // change me according to your root folder name
 $path = str_replace($basePath, '', $path);
 
+// for login guarding
+// Get the user's role from the session
+$role = isset($_SESSION['user']) ? $_SESSION['user']['role'] : null;
+
+// Define the URLs for each role
+$keywords = [
+    'Product Order' => ['url' => '/po/', 'default' => '/po/dashboard'],
+    'Human Resources' => ['url' => '/hr/', 'default' => '/hr/dashboard'],
+    'Point of Sales' => ['url' => '/sls/', 'default' => '/sls/Dashboard'],
+    'Inventory' => ['url' => '/inv/', 'default' => '/inv/main'],
+    'Finance' => ['url' => '/fin/', 'default' => '/fin/dashboard'],
+    'Delivery' => ['url' => '/dlv/', 'default' => '/dlv/dashboard'],
+];
+
+if ($role === null || !isset($keywords[$role]) ) {
+    $path = '';
+} else {
+    if (strpos($currentUri, $keywords[$role]['url']) === false) {
+        $path = $keywords[$role]['default'];
+    }
+}
+// end guard login
+
 foreach ($routes as $route => $action) {
     if (strpos($route, '{') !== false) {
         // This is a dynamic route
