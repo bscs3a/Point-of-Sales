51d90e9f4d1f0394ea6b1f68ef5f6cf5314f6928
aries joseph tagle <ajtagle12@gmail.com>
2024-05-15 13:38:31 +0800

    cleaning

--- public/finance/views/components/sidebar.php
+++ public/finance/views/components/sidebar.php
@@ -18,87 +18,7 @@
             </a>
         </li>
 
-        <!-- button dropdown -->
-        <script>
-            document.addEventListener('DOMContentLoaded', function () {
-                // document.querySelector('.toggle-reports').addEventListener('click', function () {
-                //     // document.getElementById('reports').classList.toggle('hidden');
-                //     document.getElementById('ledger').classList.add('hidden');
-                //     document.getElementById('request').classList.add('hidden');
-                //     document.getElementById('funds').classList.add('hidden');
-                //     // document.getElementById('reports-button').classList.toggle('bg-slate-400');
-
-                // });
-
-                document.querySelector('.toggle-ledger').addEventListener('click', function () {
-                    document.getElementById('ledger').classList.toggle('hidden');
-                    // document.getElementById('reports').classList.add('hidden');
-                    document.getElementById('request').classList.add('hidden');
-                    document.getElementById('funds').classList.add('hidden');
-                    // document.getElementById('reports-button').classList.toggle('bg-slate-400');
-                });
-
-                document.querySelector('.toggle-request').addEventListener('click', function () {
-                    document.getElementById('ledger').classList.add('hidden');
-                    // document.getElementById('reports').classList.add('hidden');
-                    document.getElementById('request').classList.toggle('hidden');
-                    document.getElementById('funds').classList.add('hidden');
-                    // document.getElementById('reports-button').classList.toggle('bg-slate-400');
-                });
-
-                document.querySelector('.toggle-funds').addEventListener('click', function () {
-                    document.getElementById('ledger').classList.add('hidden');
-                    // document.getElementById('reports').classList.add('hidden');
-                    document.getElementById('request').classList.add('hidden');
-                    document.getElementById('funds').classList.toggle('hidden');
-                    // document.getElementById('reports-button').classList.toggle('bg-slate-400');
-                });
-            });
-        </script>
-
-
-        <!-- <li class="mb-1  rounded-xl">
-            <button id="reports-button"
-                class="toggle-reports flex items-center py-2 px-4 w-full text-white hover:text-black  hover:bg-slate-400 rounded-xl">
-                <i class="ri-file-edit-fill mr-3 text-lg"></i>
-                <span class="text-sm font-medium">Reports</span>
-                <i class="ri-arrow-down-s-line ml-auto"></i>
-            </button>
-            <ul id="reports" class="ml-8 hidden">
-                <li>
-                    <a route='/fin/reportIncome' class="flex items-center py-2 px-4 text-white hover:text-black">
-
-                        <span class="text-sm font-medium">Income</span>
-                        <i class="ri-arrow-right-s-line ml-auto"></i>
-                    </a>
-                </li>
-                <li>
-                    <a route="/fin/reportCash" class="flex items-center py-2 px-4 text-white hover:text-black">
-
-                        <span class="text-sm font-medium">Cash Flow</span>
-                        <i class="ri-arrow-right-s-line ml-auto"></i>
-                    </a>
-                </li>
-
-                <li>
-                    <a route="/fin/reportEquity" class="flex items-center py-2 px-4 text-white hover:text-black">
-
-                        <span class="text-sm font-medium">Equity</span>
-                        <i class="ri-arrow-right-s-line ml-auto"></i>
-                    </a>
-                </li>
-
-                <li>
-                    <a route="/fin/reportBalance" class="flex items-center py-2 px-4 text-white hover:text-black">
-
-                        <span class="text-sm font-medium">Balance Sheet</span>
-                        <i class="ri-arrow-right-s-line ml-auto"></i>
-                    </a>
-                </li>
-
-
-            </ul>
-        </li> -->
+        
 
         <li class="mb-1">
             <button
@@ -120,35 +40,6 @@
                         class="flex items-center py-2 px-4 text-white hover:text-black">
 
                         <span class="text-sm font-medium">Account</span>
-                        <i class="ri-arrow-right-s-line ml-auto"></i>
-                    </a>
-                </li>
-            </ul>
-        </li>
-
-        <li class="mb-1">
-            <button
-                class="toggle-request flex items-center py-2 px-4 w-full text-white hover:bg-slate-400 hover:text-black rounded-xl">
-                <i class="ri-shake-hands-fill mr-3 text-lg"></i>
-                <span class="text-sm font-medium">Request</span>
-                <i class="ri-arrow-down-s-line ml-auto"></i>
-            </button>
-            <ul id="request" class="ml-8 hidden">
-                <li>
-                    <a route='/fin/expense' class="flex items-center py-2 px-4 text-white hover:text-black">
-                        <span class="text-sm font-medium">Expenses</span>
-                        <i class="ri-arrow-right-s-line ml-auto"></i>
-                    </a>
-                </li>
-                <li>
-                    <a route='/fin/request' class="flex items-center py-2 px-4 text-white hover:text-black">
-                        <span class="text-sm font-medium">Inventory</span>
-                        <i class="ri-arrow-right-s-line ml-auto"></i>
-                    </a>
-                </li>
-                <li>
-                    <a route='/fin/salary' class="flex items-center py-2 px-4 text-white hover:text-black">
-                        <span class="text-sm font-medium">Salary</span>
                         <i class="ri-arrow-right-s-line ml-auto"></i>
                     </a>
                 </li>
@@ -186,20 +77,19 @@
 </div>
 
 <div class="fixed top-0 left-0 w-full h-full z-40 md:hidden sidebar-overlay"></div>
-
 <!-- button dropdown -->
 <script>
-    // document.addEventListener('DOMContentLoaded', function () {
-    //     document.querySelector('.toggle-ledger').addEventListener('click', function () {
-    //         document.getElementById('ledger').classList.toggle('hidden');
-    //         document.getElementById('request').classList.add('hidden');
-    //         // document.getElementById('reports-button').classList.toggle('bg-slate-400');
-    //     });
-    //     document.querySelector('.toggle-request').addEventListener('click', function () {
-    //         document.getElementById('ledger').classList.add('hidden');
-    //         document.getElementById('request').classList.toggle('hidden');
-    //         // document.getElementById('reports-button').classList.toggle('bg-slate-400');
-    //     });
-    // });
+    document.addEventListener('DOMContentLoaded', function () {
+
+        document.querySelector('.toggle-ledger').addEventListener('click', function () {
+            document.getElementById('ledger').classList.toggle('hidden');
+            document.getElementById('funds').classList.add('hidden');
+            // document.getElementById('reports-button').classList.toggle('bg-slate-400');
+        });
+
+        document.querySelector('.toggle-funds').addEventListener('click', function () {
+            document.getElementById('ledger').classList.add('hidden');
+            document.getElementById('funds').classList.toggle('hidden');
+        });
+    });
 </script>
-<!-- End: Sidebar -->
