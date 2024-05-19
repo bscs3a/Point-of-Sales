c2e94c7860de48ca61f206335ab94352a4c61011
Isarakyun <119465506+Isarakyun@users.noreply.github.com>
2024-05-20 01:03:34 +0800

    reverting some changes

--- index.php
+++ index.php
@@ -25,8 +25,8 @@ Router::post('/login', function(){
     $user = $stmt->fetch();
 
     $base_url = 'Master'; // Define your base URL here
-    // if ($user && password_verify($password, $user['password'])) {
-    if ($user && $password == $user['password']) { // ung passwords namin from HR di naka-hash HAUSHDASDH
+    if ($user && password_verify($password, $user['password'])) {
+    // if ($user && $password == $user['password']) { // ung passwords namin from HR di naka-hash HAUSHDASDH
         $_SESSION['user'] = array();
         // Password is correct
         $_SESSION['user']['account_id'] = $user['id'];
