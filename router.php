89df84bfbd8e0851a93c176f5410b001ba5dbfcf
aries joseph tagle <ajtagle12@gmail.com>
2024-05-31 12:46:53 +0800

    fixed finance -> master links/basepath

--- router.php
+++ router.php
@@ -80,7 +80,7 @@ class Router
 
     private static function redirect($user){
         $_SESSION['pageNotFound'] = true;
-        $base_url = 'Finance'; // Define your base URL here
+        $base_url = 'Master'; // Define your base URL here
         if ($user == 'Product Order') {
             header("Location: /$base_url/po/dashboard");
             exit();
